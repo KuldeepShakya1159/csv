dc0bd7ac8f6153df1968df9bbcfabd71d9eefeeb
KuldeepShakya1159 <kuldeep1159.be20@chitkarauniversity.edu.in>
2024-09-01 13:22:28 +0530

    vercel push

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const imgComporessJobRoutes = require('./routes/ImgCompressJobRoutes');
 
 app.use('/job',imgComporessJobRoutes);
 
-
+app.get('/',(req,res)=>{
+    res.send('server is working')
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`Server started at ${process.env.PORT}`);
